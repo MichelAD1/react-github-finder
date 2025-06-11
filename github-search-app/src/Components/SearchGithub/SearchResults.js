5d2d2f099ac56ff79323e56d7b24a642709bc9c6
MichelAD1 <michel.a.abidaoud@gmail.com>
2025-06-11 23:34:39 +0300

    Creating repo card design

--- github-search-app/src/Components/SearchGithub/SearchResults.js
+++ github-search-app/src/Components/SearchGithub/SearchResults.js
@@ -1,16 +1,21 @@
 import React from "react";
 import UserCard from "./UserCard";
+import RepoCard from "./RepoCard";
 
 const SearchResults = ({ users }) => {
-  if (!users) {
+  if (!users || users.length === 0) {
     return <p className="text-center mt-6 text-gray-400">No results found.</p>;
   }
 
   return (
     <div className="mt-6 flex flex-wrap gap-6 justify-center">
-      {users.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      {users.map((item) =>
+        item.login ? (
+          <UserCard key={item.id} user={item} />
+        ) : (
+          <RepoCard key={item.id} repo={item} />
+        )
+      )}
     </div>
   );
 };
