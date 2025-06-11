b8ca5f7148b69d3e9a0484d447fc1d498a79bc28
MichelAD1 <michel.a.abidaoud@gmail.com>
2025-06-11 23:42:06 +0300

    Fixing design for no results found

--- github-search-app/src/Components/SearchGithub/SearchResults.js
+++ github-search-app/src/Components/SearchGithub/SearchResults.js
@@ -1,10 +1,18 @@
 import React from "react";
 import UserCard from "./UserCard";
 import RepoCard from "./RepoCard";
+import { FaSearch } from "react-icons/fa";
 
 const SearchResults = ({ users }) => {
   if (!users || users.length === 0) {
-    return <p className="text-center mt-6 text-gray-400">No results found.</p>;
+    return (
+      <div className="flex flex-col items-center justify-center mt-10 text-center text-white">
+        <FaSearch className="text-4xl text-purple-500 mb-4" />
+        <p className="text-lg font-medium text-gray-300">
+          No results found. Try a different search.
+        </p>
+      </div>
+    );
   }
 
   return (
