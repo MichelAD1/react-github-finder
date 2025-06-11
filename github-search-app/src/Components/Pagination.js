08db1c432c6391d0ed26065c8015b64e335d5c1d
MichelAD1 <michel.a.abidaoud@gmail.com>
2025-06-12 01:04:00 +0300

    Fixing responsiveness issue

--- github-search-app/src/Components/Pagination.js
+++ github-search-app/src/Components/Pagination.js
@@ -32,7 +32,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   }, [currentPage, totalPages]);
 
   return (
-    <div className="flex gap-2 justify-center mt-6">
+    <div className="flex flex-wrap gap-2 justify-center mt-6 px-4">
       {pageGroup > 0 && (
         <button
           onClick={handlePrevGroup}
